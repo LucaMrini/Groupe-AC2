250858e5185031a25e573e6205a3f39be346bea4
LucaMrini <148770350+LucaMrini@users.noreply.github.com>
2023-10-30 13:49:59 +0100

    added testac.r

---
+++ testAC.R
@@ -0,0 +1,7 @@
+df <- read.csv("/Users/lucamarini/Desktop/Data Science in Business analytics/2021-2022 Football Player Stats 2.csv",
+               header = TRUE,sep = ";" ,locale = locale(encoding = "UTF-8"))
+View(df)
+dftest = df[1:100,c(2:15)]
+dftest2 <- df$Age * df$Min
+dftest$Shots*90
+View(dftest2)
